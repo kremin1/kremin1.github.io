df15cfad92320adbb80b51fdfdef8ae89cd87813
kremin1 <mnunn654321@gmail.com>
2025-09-01 18:03:40 +1000

    fix?

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-
-# Example: Python function that returns data
 @app.route("/api/square", methods=["POST"])
 def square():
-    data = request.get_json()  # get { "number": 5 }
-    n = data["number"]
-    return jsonify({"result": n * n})
+    n = request.json["number"]
+    return jsonify({"result": n*n})
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
